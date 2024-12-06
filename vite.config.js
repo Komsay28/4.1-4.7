d52942e008835a16981d5d1483c5291610a9f0fe
Komsay28 <leocarlballadares1@gmail.com>
2024-12-06 11:30:14 +0800

    fix: Update configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 export default {
   root: 'src/',
   publicDir: '../static/',
-  base: './',
+  base: '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
     sourcemap: true
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
   }
 }
