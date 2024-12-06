a1f043e59594a148d5d1a2560c57d4530bbabe7a
Komsay28 <leocarlballadares1@gmail.com>
2024-12-06 11:26:17 +0800

    fix: Simplify build configuration for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
-
 export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: true
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true,
-        assetsDir: 'assets',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined
-            }
-        }
-    }
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 }
